4755ffed4f08ed72d548bd63460908c0d4accf4a
lucakozak <lucakozak@icloud.com>
2017-09-22 15:18:17 +0200

    git status

---
+++ main 2.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  09.22.9
+//
+//  Created by Kozak, Luca on 2017. 09. 22..
+//  Copyright © 2017. Kozak, Luca. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+   
+    int a;
+    int b;
+    int sum;
+    int difference;
+    int product;
+    float quotient;
+    
+    printf("Write two numbers separated with comma");
+    scanf("%d,%d", &a, &b);
+    
+    sum=a+b;
+    difference=a-b;
+    product=a*b;
+    quotient=a/b;
+    
+    printf("The sum of the given numbers:%d\n", sum);
+    printf("The difference of the given numbers:%d\n", difference);
+    printf("The product of the given numbers:%d\n", product);
+    printf("The quotient of the given numbers:%f\n", quotient);
+    
+    return 0;
+    
+}
